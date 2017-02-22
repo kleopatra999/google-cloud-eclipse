30e8bfcf9c056af8230094f32180ba9ffbef6e48
Abbie Farr <afarr@google.com>
2017-02-22 15:58:00 -0500

    problems view quick fix reads from editor

--- plugins/com.google.cloud.tools.eclipse.appengine.validation/src/com/google/cloud/tools/eclipse/appengine/validation/ApplicationQuickFix.java
+++ plugins/com.google.cloud.tools.eclipse.appengine.validation/src/com/google/cloud/tools/eclipse/appengine/validation/ApplicationQuickFix.java
@@ -16,7 +16,10 @@
 
 package com.google.cloud.tools.eclipse.appengine.validation;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -24,8 +27,16 @@ import javax.xml.transform.TransformerException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IMarkerResolution;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.ResourceUtil;
 import com.google.cloud.tools.eclipse.util.Xslt;
+import com.google.common.io.CharStreams;
 
 /**
  * Applies application.xsl to appengine-web.xml to remove an <application/> element.
@@ -40,15 +51,50 @@ public class ApplicationQuickFix implements IMarkerResolution {
     return Messages.getString("remove.application.element");
   }
 
+  /**
+   * Attempts to edit the {@link IDocument} in the open editor. If the editor is not open,
+   * reads the file from memory and transforms in place.
+   */
   @Override
   public void run(IMarker marker) {
     try {
       IFile file = (IFile) marker.getResource();
+      IDocument document = getCurrentDocument(file);
       URL xslPath = ApplicationQuickFix.class.getResource("/xslt/application.xsl");
-      Xslt.transformInPlace(file, xslPath);
-    } catch (IOException | CoreException| TransformerException ex) {
+      if (document != null) {
+        String currentContents = document.get();
+        String encoding = file.getCharset();
+        InputStream documentStream = new ByteArrayInputStream(currentContents.getBytes(encoding));
+        InputStream transformed = Xslt.applyXslt(documentStream, xslPath.openStream());
+        String newDoc = convertStreamToString(transformed, encoding);
+        document.set(newDoc);
+      } else {
+        Xslt.transformInPlace(file, xslPath);
+      }
+    } catch (IOException | TransformerException | CoreException ex) {
       logger.log(Level.SEVERE, ex.getMessage());
     }
+  }
+  
+  /**
+   * Returns {@link IDocument} in the open editor, or null if the editor
+   * is not open.
+   */
+  IDocument getCurrentDocument(IFile file) {
+    IWorkbench workbench = PlatformUI.getWorkbench();
+    IWorkbenchWindow activeWorkbenchWindow = workbench.getActiveWorkbenchWindow();
+    IWorkbenchPage activePage = activeWorkbenchWindow.getActivePage();
+    IEditorPart editorPart = ResourceUtil.findEditor(activePage, file);
+    if (editorPart != null) {
+      IDocument document = (IDocument) editorPart.getAdapter(IDocument.class);
+      return document;
+    }
+    return null;
+  }
+  
+  public static String convertStreamToString(InputStream is, String charset) throws IOException {
+    String result = CharStreams.toString(new InputStreamReader(is, charset));
+    return result;
   }
   
 }
